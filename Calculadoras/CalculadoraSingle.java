eee86cf5d55a557c20d4bfaf9f4079570e802c1f
MelissaPerez09 <per21385@uvg.edu.gt>
2022-03-04 15:32:26 -0600

    Calculadora Single

---
+++ Calculadoras/CalculadoraSingle.java
@@ -0,0 +1,95 @@
+package Calculadoras;
+
+/**
+ * Esta es la clase que implementa la interfaz de la calculadora Infix.
+ * Analiza una línea a la vez.
+ * Universidad del Valle de Guatemala
+ * @author Mark Albrand
+ * @author Jimena Hernández
+ * @author Emily Pérez
+ * @version 2-mar-22
+ */
+
+public class CalculadoraSingle implements InFixCalc{
+    private static CalculadoraSingle instance;
+
+    public static CalculadoraSingle getInstance(){
+        if(instance == null){
+            return new CalculadoraSingle();
+        } 
+        else{
+            return instance;
+        }
+    }
+
+    @Override
+    /**
+     * Evaluate una linea en formato infix
+     * @param expression Linea en formato postfix
+     * @return Resultado
+     */
+    public int Evaluate(String expression) {
+        String[] lineaActual = expression.split(" ");
+        SingleLinkedList<Integer> calculadora = new SingleLinkedList<>();
+
+        Integer resultado = 0;
+        for (int j = 0; j < lineaActual.length; j++) {
+            String c = lineaActual[j];
+
+            try {
+                int element = Integer.parseInt(c);
+                calculadora.InsertAtStart(element);
+            } catch (Exception e) {
+                int n = calculadora.Count() - 1;
+
+                if(n >= 1){
+                    switch (c) {
+                        case "+":
+                            Integer operando1 = calculadora.DeleteAtStart();
+                            for (int k = 0; k < n; k++) {
+                                operando1 = operando1 + calculadora.DeleteAtStart();
+                            }
+                            resultado = operando1;
+                            calculadora.InsertAtStart(resultado);
+                            break;
+
+                        case "-":
+                            Integer operando2 = calculadora.DeleteAtStart();
+                            for (int k = 0; k < n; k++) {
+                                operando2 = operando2 - calculadora.DeleteAtStart();
+                            }
+                            resultado = operando2;
+                            calculadora.InsertAtStart(resultado);
+                            break;
+
+                        case "*":
+                            Integer operando3 = calculadora.DeleteAtStart();
+                            for (int k = 0; k < n; k++) {
+                                operando3 = operando3 * calculadora.DeleteAtStart();
+                            }
+                            resultado = operando3;
+                            calculadora.InsertAtStart(resultado);
+                            break;
+
+                        case "/":
+                            Integer operando4 = calculadora.DeleteAtStart();
+                            for (int k = 0; k < n; k++) {
+                                operando4 = operando4 / calculadora.DeleteAtStart();
+                            }
+                            resultado = operando4;
+                            calculadora.InsertAtStart(resultado);
+                            break;
+
+                        default:
+                            resultado = -1;
+                            break;
+                    }
+                }
+                else{
+                    resultado = calculadora.DeleteAtStart();  // Si ya no quedan operandos
+                }
+            }
+        }
+        return resultado;
+    }
+}
